513d528aa3546b944274d1c20a633ba4afc72cd1
mikalai <mikalai.hurynovich@itechart-group.com>
2023-01-23 22:03:38 +0300

    feat: implemented draw_circle

--- src/service/events.ts
+++ src/service/events.ts
@@ -1,4 +1,4 @@
-import { mouse, left, right, up, down } from '@nut-tree/nut-js'
+import { mouse, left, right, up, down, Point, Button, straightTo } from '@nut-tree/nut-js'
 
 export const events = async (chunk: string) => {
   const [command, width, height] = chunk.toString().split(' ');
@@ -9,9 +9,26 @@ export const events = async (chunk: string) => {
     return await mouse.move(right(+width))
     }
   if (command === 'mouse_up') {
-    return await mouse.move(up(+width))
+    await mouse.move(up(+width));
+    return
   }
   if (command === 'mouse_down') {
-    return await mouse.move(down(+width))
+    await mouse.move(down(+width));
+    return;
+  }
+  if (command === 'draw_circle') {
+    const center = await mouse.getPosition()
+    const radius = +width;
+    mouse.config.mouseSpeed = 300
+    await mouse.pressButton(Button.LEFT)
+
+    for (let angle = 0; angle < 360; angle += 1) {
+      const x = center.x - radius * Math.cos((angle * Math.PI) / 180) + radius;
+      const y = center.y - radius * Math.sin((angle * Math.PI) / 180);
+      await mouse.move(straightTo(new Point(x, y)));
+    }
+
+    await mouse.releaseButton(Button.LEFT)
+    return
   }
 }
