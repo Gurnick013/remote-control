aecda8f5ba0a6b2a06de857263193e8a738cc740
mikalai <mikalai.hurynovich@itechart-group.com>
2023-01-23 23:52:59 +0300

    fix: a bit refactoring

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,21 @@ const server = httpServer;
 const wss = new WebSocketServer({ port: WSS_PORT });
 
 wss.on('connection', (ws) => {
-  console.log('Connected')
+  console.log('Connected');
   const stream = createWebSocketStream(ws, {encoding: 'utf8', decodeStrings: false })
   stream.on('data', async (chunk) => {
     await events(chunk);
-    if (chunk.toString() === 'mouse_position') {
-      const pos = await mouse.getPosition()
-      stream.write(`mouse_position ${pos.x},${pos.y}`)
-    } else if (chunk.toString() === 'prnt_scrn') {
-      await printScreen(stream);
-    } else {
-      stream.write(chunk.toString().replace(/ /g,"_"))
+    try {
+      if (chunk.toString() === 'mouse_position') {
+        const pos = await mouse.getPosition();
+        stream.write(`mouse_position ${pos.x},${pos.y}`)
+      } else if (chunk.toString() === 'prnt_scrn') {
+        await printScreen(stream);
+      } else {
+        stream.write(chunk.toString().replace(/ /g,"_"))
+      }
+    } catch (e) {
+      console.log(`Error: ${e}`)
     }
     console.log(`Last Command: ${chunk}`)
   })
@@ -36,4 +40,4 @@ process.on('SIGINT', () => {
 })
 
 wss.on('close', () => console.log('Connection is closed'));
-wss.on('error', (e) => console.log(`Error: ${e.message}`))
+wss.on('error', (e) => console.log(`Error: ${e.message}`));
